552454a4c20986ba413426f2b89b1dd1041affb2
OnSwitchOff <kassov@ukr.net>
2022-10-14 09:52:33 +0300

    correct font-name

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -22,12 +22,13 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor: UIColor.black,
         NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondenseBlack", size: 30)!,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 30)!,
         NSAttributedString.Key.strokeWidth: 2
     ]
     
     func prepareTextField(textField: UITextField, defaultText: String){
         textField.defaultTextAttributes = memeTextAttributes
+        textField.text = defaultText
         textField.delegate = self
     }
     
